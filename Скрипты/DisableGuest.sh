eff5ae50d4624669be457a38860d6d89742c6f3c
accidentalZero <59977759+accidentalZero@users.noreply.github.com>
2024-11-14 16:26:59 +0300

    Update DisableGuest.sh

--- Скрипты/DisableGuest.sh
+++ Скрипты/DisableGuest.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+# Скрипт отключает кнопки авторизации под гостевым пользователем
 
+sudo -v
+if [ $? -eq 0 ]; then
+sudo sed -i '/# Hidden users/{
+  /^HideUsers=guest/b
+  a\HideUsers=guest
+}' /etc/sddm.conf.d/50-default.conf
+  sudo sed -i 's/#guest-enabled=true/guest-enabled=false/g' /etc/mos-auth/mos-auth.conf
+  echo "Выполнение завершено!"
+else
+  echo "Используйте учетную запись с правами администратора!"
+fi
