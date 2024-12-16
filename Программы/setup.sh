86869835c8f028db15b7a1c353fee43dae2b631c
accidentalZero <accidental0zero@gmail.com>
2024-12-16 21:49:27 +0300

    Добавить исправления в setupLinux Imaginator

--- Программы/setup.sh
+++ Программы/setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
 # Скрипт "установки" программы Imaginator
-username=user
+username=admin
 
 sudo -v
 if [ $? -eq 0 ]; then
   sudo mkdir /opt/imaginator/
   sudo wget -O /opt/imaginator/imaginator.py https://github.com/schoolPerspective/it-support/raw/refs/heads/main/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B/imaginator.py
-  sudo wget -O /opt/imaginator/imaginator.png
+  sudo chmod +x /opt/imaginator/imaginator.py
+  sudo wget -O /opt/imaginator/imaginator.png https://raw.githubusercontent.com/schoolPerspective/it-support/refs/heads/main/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B/icon.png
   echo "
+[Desktop Entry]
 Name=Imaginator
 Exec=python /opt/imaginator/imaginator.py
 Type=Application
