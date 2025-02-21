958f27d62c09aabc75a003efd2f76243bae0244c
accidentalZero <59977759+accidentalZero@users.noreply.github.com>
2025-02-21 15:50:03 +0300

    Update SetupKDE_OGE.sh

--- Скрипты/SetupKDE_OGE.sh
+++ Скрипты/SetupKDE_OGE.sh
@@ -8,7 +8,7 @@ kwriteconfig5 --file kwinrc --group ElectricBorders --key TopLeft --delete
 kwriteconfig5 --file kwinrc --group Windows --key ElectricBorders 0
 
 # Настройка режимов работы электропитания
-cp ~/.config/powermanagementprofilesrc ~.config/powermanagementprofilesrc.bak
+cp ~/.config/powermanagementprofilesrc ~/.config/powermanagementprofilesrc.bak
 wget -O ~/.config/powermanagementprofilesrc https://github.com/schoolPerspective/it-support/raw/refs/heads/main/%D0%9A%D0%BE%D0%BD%D1%84%D0%B8%D0%B3%D0%B8/%D0%9E%D0%93%D0%AD/powermanagementprofilesrc
 
 # Убираем лишнее с рабочего стола
